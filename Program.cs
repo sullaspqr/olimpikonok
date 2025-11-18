b2b414e2446268e238a9f30c78201e3daeba8c42
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-11-18 21:40:49 +0100

    Validate CONNECTION_STRING before setting up DbContext
    Added validation for CONNECTION_STRING environment variable.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,16 @@ namespace OlimpikonokAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connStr))
+            {
+                throw new Exception("CONNECTION_STRING environment variable is not set!");
+            }
 
-builder.Services.AddDbContext<OlimpikonokContext>(options =>
-    options.UseMySQL(connStr)
-);
+            // DbContext hozzáadása
+            builder.Services.AddDbContext<OlimpikonokContext>(options =>
+                options.UseMySQL(connStr)  // hivatalos MySQL provider
+            );
             builder.Services.AddControllers();
             builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
