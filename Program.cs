6f5bb00dcdcd0f832a258de4dd92ea6d1aca6bdc
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-11-18 22:02:20 +0100

    Refactor database connection and add CORS policy
    Updated the database connection setup and added CORS policy.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
-using Microsoft.EntityFrameworkCore;
 using OlimpikonokAPI.Models;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.AspNetCore.Cors;
 
 namespace OlimpikonokAPI
 {
@@ -9,24 +13,27 @@ namespace OlimpikonokAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var connStr = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-            if (string.IsNullOrEmpty(connStr))
-            {
-                throw new Exception("CONNECTION_STRING environment variable is not set!");
-            }
-
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
             // DbContext hozzáadása
-            builder.Services.AddDbContext<OlimpikonokContext>(options =>
-                options.UseMySQL(connStr)  // hivatalos MySQL provider
-            );
+            builder.Services.AddDbContext<OlimpikonokContext>(options => 
+                  options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
             builder.Services.AddControllers();
             builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    policy =>
+                    {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                    });
+            });
             var app = builder.Build();
-
+            app.UseCors("AllowAllOrigins");
             // Configure the HTTP request pipeline.
             
             app.UseSwagger();
